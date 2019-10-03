4badf8c39432c53b90546a8c2a47ae599aa90ff0
volkangurbuz <volkangurbuz16@gmail.com>
2019-10-03 16:10:49 +0200

    project updated with version 2.0

---
+++ app/src/main/java/com/volkangurbuz/stajtakip/AdminMenuActivity.java
@@ -0,0 +1,13 @@
+package com.volkangurbuz.stajtakip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdminMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_menu);
+    }
+}
