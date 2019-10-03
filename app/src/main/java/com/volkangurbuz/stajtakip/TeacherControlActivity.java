623aa1a3f06a711a5360c27df81a27341bceb18d
volkangurbuz <volkangurbuz16@gmail.com>
2019-10-03 20:28:35 +0200

    small fixed

---
+++ app/src/main/java/com/volkangurbuz/stajtakip/TeacherControlActivity.java
@@ -0,0 +1,13 @@
+package com.volkangurbuz.stajtakip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeacherControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_control);
+    }
+}
