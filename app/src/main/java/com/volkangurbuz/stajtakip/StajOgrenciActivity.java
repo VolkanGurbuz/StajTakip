cf37450a7f3c05e0eb25cfeb64ca2fc8a9446457
volkangurbuz <volkangurbuz16@gmail.com>
2019-10-03 20:45:09 +0200

    added stajogrenci activity

---
+++ app/src/main/java/com/volkangurbuz/stajtakip/StajOgrenciActivity.java
@@ -0,0 +1,13 @@
+package com.volkangurbuz.stajtakip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StajOgrenciActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staj_ogrenci);
+    }
+}
