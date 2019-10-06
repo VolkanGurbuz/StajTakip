8b3ec3bde3b49dbdf1eea5351fdf5ffc3f69d588
volkangurbuz <volkangurbuz16@gmail.com>
2019-10-06 18:20:31 +0200

    setup the recipeapi interface

--- app/src/main/java/com/volkangurbuz/stajtakip/Requests/RecipeApi.java
+++ app/src/main/java/com/volkangurbuz/stajtakip/Requests/RecipeApi.java
@@ -1,3 +1,38 @@
 package com.volkangurbuz.stajtakip.Requests;
 
-public interface RecipeApi {}
+import com.volkangurbuz.stajtakip.Models.InternDate;
+import com.volkangurbuz.stajtakip.Models.Sinif;
+import com.volkangurbuz.stajtakip.Models.Student;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface RecipeApi {
+
+  @GET("product/readAllStudentIntern.php")
+  Call<ResponseBody> getAllStudentIntern();
+
+  @POST("product/updateIntern.php")
+  Call<Student> updateIntern(@Body Student student);
+
+  @GET("product/readInternAll.php")
+  Call<InternDate> getAllIntern();
+
+  @GET("product/readInternAllOgrenci.php")
+  Call<Student> getAllInternOgrenci();
+
+  @GET("product/readClassAll.php")
+  Call<Sinif> getClassAll();
+
+  // TODO
+  /*
+    develop a searchResponse
+      @GET("api/search")
+  Call<RecipeSearchResponse> searchRecipe(
+      @Query("key") String key, @Query("q") String query, @Query("page") String page);
+     */
+
+}
